4c676925226090d7e56858588a69958e995c8782
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-09-16 16:02:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a bot to handle a ArkServer running on the same server
+You will need a server Folder containing 2 .bat file
+One name server.bat starting you server with the desired parameter
+One name killserv.bat killing ShooterGameServer.exe
+You also need a config.json file containing :
+"token": your discord bot token here,
+"clientId": your client Id here,
+"guildId": your guild Id here,
+"ip": the ip of the arc server,
+"password": the password of the ark server,
+"port": the port of the RCON port
---
+++ index.js
@@ -0,0 +1,74 @@
+const fs = require("node:fs"); // File system module
+const path = require("node:path"); // Path module
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js"); // Discord.js library
+const { token } = require("./config.json");
+const Function = require("./function/function"); // Importing custom function module
+
+let ServerStatus = false; //Server State
+let RconAuth = false; //Rcon Auth State
+let ServerStarting = false; //Server Starting state
+
+global.client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ], // Setting Discord bot intents
+});
+
+client.once(Events.ClientReady, async (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+  try {
+    RconAuth = await Function.server_status(RconAuth);
+    ServerStatus = RconAuth;
+    console.log("Server Status " + ServerStatus);
+  } catch (error) {
+    throw error;
+  }
+});
+
+/* HANDELING OF COMMAND FOR DISCORDBOT */
+client.commands = new Collection(); // Creating a collection for bot commands
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js")); // Reading and filtering command files
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.login(token); // Logging in with the bot's token
+module.exports = { ServerStatus, ServerStarting, RconAuth };
